5ae9ede784ecafff76bcd2219ec39b225181bd41
retiq <idzkowskit@gmail.com>
2026-03-12 22:15:44 +0100

    big refactor: made model loss agnostic(added heads that would work as adapters between the model and losses), made NegativeSamplingLoss a pure function(as it should); smaller refactor edits in between

--- src/model/loss.py
+++ src/model/loss.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 import numpy as np
 from src.utils.math_utils import sigmoid
 
@@ -13,12 +13,12 @@ class Loss(ABC):
     def backward(self):
         pass
 
-
-@dataclass(slots=True, init=False)
-class CrossEntropyLoss(Loss):
     def __call__(self, logits, targets):
         return self.forward(logits, targets)
 
+
+@dataclass(slots=True, init=False)
+class CrossEntropyLoss(Loss):
     def forward(self, logits, targets):
         """Logits and centering used for numerical stability"""
         self.targets = targets
@@ -43,80 +43,27 @@ class CrossEntropyLoss(Loss):
 
 
 @dataclass(slots=True)
-class NegativeSamplingLoss:
-    word_counts: np.ndarray
-    k: int = 10
-    vocab_size: int = field(init=False)
-    W_out: np.ndarray | None = field(init=False, default=None)
-    noise_dist: np.ndarray = field(init=False)
-    delta_W_out: np.ndarray = field(init=False)
-    cache: dict = field(init=False, default_factory=dict)
+class NegativeSamplingLoss(Loss):
+    def forward(self, head_output: dict, targets=None) -> float:
+        pos_dots = head_output["pos_dots"]
+        neg_dots = head_output["neg_dots"]
 
-    def __post_init__(self):
-        self.vocab_size = len(self.word_counts)
-        smoothed = np.power(self.word_counts.astype(np.float64), 0.75)
-        self.noise_dist = smoothed / smoothed.sum()
-
-    def __call__(self, h, targets):
-        return self.forward(h, targets)
-
-    def _ensure_w_out(self, n_dim):
-        if self.W_out is None:
-            scale = 1.0 / np.sqrt(n_dim)
-            self.W_out = np.random.randn(self.vocab_size, n_dim) * scale
-
-    def forward(self, h, targets):
-        self._ensure_w_out(h.shape[1])
-        neg_ids = np.random.choice(
-            self.vocab_size, size=(len(targets), self.k), p=self.noise_dist
-        )
-        v_pos = self.W_out[targets]
-        v_neg = self.W_out[neg_ids]
-        pos_dots = np.sum(h * v_pos, axis=1)
-        neg_dots = np.einsum("bd,bkd->bk", h, v_neg)
-        neg_dots = np.sum(h[:, None, :] * v_neg, axis=2)
         sig_pos = sigmoid(pos_dots)
         sig_neg = sigmoid(neg_dots)
+
+        self.cache = {"sig_pos": sig_pos, "sig_neg": sig_neg}
+
         loss = -np.mean(
             np.log(sig_pos + 1e-9) + np.sum(np.log(1 - sig_neg + 1e-9), axis=1)
         )
-        self.cache = {
-            "h": h,
-            "targets": targets,
-            "neg_ids": neg_ids,
-            "v_pos": v_pos,
-            "v_neg": v_neg,
-            "sig_pos": sig_pos,
-            "sig_neg": sig_neg,
-        }
         return loss
 
     def backward(self):
-        h = self.cache["h"]
-        targets = self.cache["targets"]
-        neg_ids = self.cache["neg_ids"]
-        v_pos = self.cache["v_pos"]
-        v_neg = self.cache["v_neg"]
         sig_pos = self.cache["sig_pos"]
         sig_neg = self.cache["sig_neg"]
-        B = len(targets)
+
         g_pos = sig_pos - 1
+
         g_neg = sig_neg
-        delta_H = g_pos[:, None] * v_pos + np.sum(g_neg[:, :, None] * v_neg, axis=1)
-        # delta_H /= B
-        self.delta_W_out = np.zeros_like(self.W_out)
-        np.add.at(self.delta_W_out, targets, g_pos[:, None] * h)
-        D = self.W_out.shape[1]
-        np.add.at(
-            self.delta_W_out,
-            neg_ids.ravel(),
-            (g_neg[:, :, None] * h[:, None, :]).reshape(-1, D),
-        )
-        # self.delta_W_out /= B
-        return delta_H
 
-    def params(self):
-        return [self.W_out]
-
-    def grads(self):
-        return [self.delta_W_out]
+        return {"g_pos": g_pos, "g_neg": g_neg}
--- src/model/model.py
+++ src/model/model.py
@@ -1,5 +1,6 @@
 import numpy as np
 from abc import abstractmethod, ABC
+from dataclasses import dataclass, field
 
 
 class Model(ABC):
@@ -20,13 +21,125 @@ class Model(ABC):
         pass
 
 
-class EmbeddingModel(Model):
+class Encoder(Model):
     @abstractmethod
     def embed(self, *args, **kwargs):
         pass
 
 
-class CBOW(EmbeddingModel):
+class Head(Model):
+    pass
+
+
+@dataclass(slots=True)
+class LinearProjectionHead(Model):
+    in_dim: int
+    out_dim: int
+    W_out: np.ndarray = field(init=False)
+    delta_W_out: np.ndarray = field(init=False)
+    cache: dict = field(init=False, default_factory=dict)
+
+    def __post_init__(self):
+        scale = 1.0 / np.sqrt(self.in_dim)
+        self.W_out = np.random.randn(self.out_dim, self.in_dim) * scale
+
+    def forward(self, h, targets=None):
+        self.cache["h"] = h
+        return h @ self.W_out.T
+
+    def backward(self, delta_Z):
+        h = self.cache["h"]
+
+        self.delta_W_out = delta_Z.T @ h
+
+        delta_H = delta_Z @ self.W_out
+
+        return delta_H
+
+    def params(self):
+        return [self.W_out]
+
+    def grads(self):
+        return [self.delta_W_out]
+
+
+@dataclass(slots=True)
+class NegativeSamplingHead(Model):
+    in_dim: int
+    vocab_size: int
+    word_counts: np.ndarray
+    k: int = 10
+
+    W_out: np.ndarray | None = field(init=False, default=None)
+    noise_dist: np.ndarray = field(init=False)
+    delta_W_out: np.ndarray = field(init=False)
+    cache: dict = field(init=False, default_factory=dict)
+
+    def __post_init__(self):
+        smoothed = np.power(self.word_counts.astype(np.float64), 0.75)
+        self.noise_dist = smoothed / smoothed.sum()
+
+    def _ensure_w_out(self, n_dim):
+        if self.W_out is None:
+            self.W_out = np.random.uniform(
+                -0.5 / n_dim, 0.5 / n_dim, (self.vocab_size, n_dim)
+            )
+
+    def forward(self, h, targets=None):
+        if targets is None:
+            raise ValueError("NegativeSamplingHead requires targets in forward pass")
+
+        self._ensure_w_out(h.shape[1])
+        neg_ids = np.random.choice(
+            self.vocab_size, size=(len(targets), self.k), p=self.noise_dist
+        )
+        v_pos = self.W_out[targets]
+        v_neg = self.W_out[neg_ids]
+        pos_dots = np.sum(h * v_pos, axis=1)
+        neg_dots = np.einsum("bd,bkd->bk", h, v_neg)
+        self.cache = {
+            "h": h,
+            "targets": targets,
+            "neg_ids": neg_ids,
+            "v_pos": v_pos,
+            "v_neg": v_neg,
+        }
+
+        return {"pos_dots": pos_dots, "neg_dots": neg_dots}
+
+    def backward(self, score_grads):
+        g_pos = score_grads["g_pos"]
+        g_neg = score_grads["g_neg"]
+
+        h = self.cache["h"]
+        targets = self.cache["targets"]
+        neg_ids = self.cache["neg_ids"]
+        v_pos = self.cache["v_pos"]
+        v_neg = self.cache["v_neg"]
+
+        delta_H = g_pos[:, None] * v_pos + np.sum(g_neg[:, :, None] * v_neg, axis=1)
+
+        self.delta_W_out = np.zeros_like(self.W_out)
+
+        np.add.at(self.delta_W_out, targets, g_pos[:, None] * h)
+
+        D = self.W_out.shape[1]
+        np.add.at(
+            self.delta_W_out,
+            neg_ids.ravel(),
+            (g_neg[:, :, None] * h[:, None, :]).reshape(-1, D),
+        )
+        return delta_H
+
+    def params(self):
+        return [self.W_out] if self.W_out is not None else []
+
+    def grads(self):
+        return [self.delta_W_out] if hasattr(self, "delta_W_out") else []
+
+
+@dataclass(slots=True)
+class CBOWEncoder(Encoder):
     """
     The model consits of 3 layers:
         input: 2*window_size
@@ -39,12 +152,15 @@ class CBOW(EmbeddingModel):
         (B, W*2) -> <proccess each one by one>                           -> (B, V)
     """
 
-    def __init__(self, vocab_size: int, n_dim: int = 300):
-        self.vocab_size = vocab_size
-        self.n_dim = n_dim
+    vocab_size: int
+    n_dim: int = 300
 
-        scale = 1.0 / np.sqrt(n_dim)
-        self.embeddings = np.random.randn(vocab_size, n_dim) * scale
+    embeddings: np.ndarray = field(init=False)
+    cache: dict = field(init=False, default_factory=dict)
+
+    def __post_init__(self):
+        scale = 1.0 / np.sqrt(self.n_dim)
+        self.embeddings = np.random.randn(self.vocab_size, self.n_dim) * scale
 
     def forward(self, batch: np.ndarray):
         """Batch is an array with -1 for padding"""
@@ -88,3 +204,29 @@ class CBOW(EmbeddingModel):
 
     def embed(self, word_id):
         return self.embeddings[word_id]
+
+
+class TrainableModel(Model):
+    encoder: Encoder
+    head: Head
+
+
+@dataclass(slots=True)
+class CBOW(TrainableModel):
+    encoder: CBOWEncoder
+    head: Head
+
+    def forward(self, batch, targets=None):
+        h = self.encoder.forward(batch)  # the model doesnt see the target
+        output = self.head.forward(h, targets)
+        return output
+
+    def backward(self, delta_output):
+        delta_H = self.head.backward(delta_output)
+        self.encoder.backward(delta_H)
+
+    def params(self):
+        return self.encoder.params() + self.head.params()
+
+    def grads(self):
+        return self.encoder.grads() + self.head.grads()
